b5bdfe8c3630d80921c4f280cce9eeeac031ffef
PC6-01\PC1-17 <sistemas@artesyletras.com.co>
2022-11-01 19:38:05 -0500

    evaluacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FotosComponent } from './componentes/fotos/fotos.component';
+import { PokemonListComponent } from './componentes/pokemon-list/pokemon-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FotosComponent
+    FotosComponent,
+    PokemonListComponent
   ],
   imports: [
     BrowserModule
