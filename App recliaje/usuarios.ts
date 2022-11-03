f15ac6f7af134a14b93c8e06e7035b5d18b0cbd1
JulianaR9 <julianavreyes06@gmail.com>
2022-11-03 16:42:48 -0500

    Usuarios App 2

---
+++ App recliaje/usuarios.ts
@@ -0,0 +1,17 @@
+interface Usuario {
+    Nombre:string,
+    Dirección:string,
+    Telefono:string,
+    Dia:string,
+    Hora:string,
+  
+}
+
+let usuario: Usuario =
+{
+    Nombre:"Ana Rodriguez",
+    Dirección:"Calle 50 A sur 28-46",
+    Telefono: "",
+    Dia:"Viernes",
+    Hora:"4:30 pm",
+}
