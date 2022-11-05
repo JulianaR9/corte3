3410d8001bd42205d47088894d8b40092b00a32e
JulianaR9 <julianavreyes06@gmail.com>
2022-11-05 17:14:21 -0500

    Usuarios App f

--- App recliaje/usuarios.ts
+++ App recliaje/usuarios.ts
@@ -14,16 +14,7 @@ let usuario1: Usuario =
     Telefono: "Tres uno uno dos cinco seis siete tres nueve dos",
     Dia:"Viernes",
     Hora:"4:30 pm",
-}
-
-
-interface Usuario {
-    Nombre:string,
-    Dirección:string,
-    Telefono:string,
-    Dia:string,
-    Hora:string,
-  
+    
 }
 
 let usuario2: Usuario =
@@ -34,5 +25,98 @@ let usuario2: Usuario =
     Dia:"Lunes",
     Hora:"6:30 pm",
 }
+
+let usuario3: Usuario =
+{
+    Nombre:"Daniel Malaver",
+    Dirección:"Carrera 38 sur 53-51",
+    Telefono: "tres uno cero seis cinco cuatro cuatro ocho ocho dos",
+    Dia:"Miércoles",
+    Hora:"2:30 pm",
+}
+
+let usuario4: Usuario =
+{
+    Nombre:"Isabel Martínez",
+    Dirección:"Calle 50 B sur 36-24",
+    Telefono: "tres cero cero dos nueve ocho siete cinco cinco cuatro ",
+    Dia:"Viernes",
+    Hora:"11:30 am",
+}
+
+let usuario5: Usuario =
+{
+    Nombre:"Joaquín Rozo",
+    Dirección:"Carrera 35 sur 50-36",
+    Telefono: "tres uno dos cinco siete cinco nueve dos uno seis",
+    Dia:"Martes",
+    Hora:"6:30 pm",
+}
+
+let usuario6: Usuario =
+{
+    Nombre:"Antonia Garzón",
+    Dirección:"Carrera 36 A sur 52-08",
+    Telefono: "tres uno uno cinco nueve siete tres dos cuatro cuatro",
+    Dia:"Miércoles",
+    Hora:"10:00 am",
+}
+
+let usuario7: Usuario =
+{
+    Nombre:"Esteban Santana",
+    Dirección:"Calle 49 B sur 35-46",
+    Telefono: "tres uno tres cuatro dos ocho cinco seis seis uno",
+    Dia:"Jueves",
+    Hora:"7:00 pm",
+}
+
+let usuario8: Usuario =
+{
+    Nombre:"Jeronimo Callejas",
+    Dirección:"Calle 50 sur 34-64",
+    Telefono: "tres uno cuatro seis dos cero ocho cero uno dos",
+    Dia:"Lunes",
+    Hora:"2:30 pm",
+}
+
+let usuario9: Usuario =
+{
+    Nombre:"Alejandra Molina",
+    Dirección:"Calle 56 sur 33-67",
+    Telefono: "tres dos cero dos cuatro ocho ocho cinco uno tres",
+    Dia:"Viernes",
+    Hora:"11:30 am",
+}
+
+let usuario10: Usuario =
+{
+    Nombre:"Andrea Velez",
+    Dirección:"Carrera 52 sur 38-40",
+    Telefono: "tres uno dos nueve seis nueve tres dos cuatro ocho",
+    Dia:"Martes",
+    Hora:"10:00 am",
+}
+
+let usuario11: Usuario =
+{
+    Nombre:"Marin Gutiérrez",
+    Dirección:"Calle 52 G sur 35-40",
+    Telefono: "tres uno cero dos ocho ocho cuatro seis dos ocho",
+    Dia:"Jueves",
+    Hora:"6:30 pm",
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 console.log(usuario1,usuario2);
